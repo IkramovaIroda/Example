54f24dbd8508eefbe9bf61cb35cfe159dd1db263
Ikramova Iroda <irodastudent@octo.uz>
2023-03-27 17:40:04 +0300

    task 922

--- src/main/java/com/example/ikramova/PDF/Reading_and_Modifying_an_Existing_PDF.java
+++ src/main/java/com/example/ikramova/PDF/Reading_and_Modifying_an_Existing_PDF.java
@@ -1,35 +1,35 @@
-package com.example.ikramova.PDF;
-import java.io.*;
-import java.net.URL;
-import java.io.FileOutputStream;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.*;
-import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfWriter;
-public class Reading_and_Modifying_an_Existing_PDF {
-    public static void main(String[] args) {
-        try
-        {
-            //Read file using PdfReader
-            PdfReader pdfReader = new PdfReader("HelloWorld.pdf");
-            //Modify file using PdfReader
-            PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileOutputStream("HelloWorld-modified.pdf"));
-            Image image = Image.getInstance("temp.jpg");
-            image.scaleAbsolute(100, 50);
-            image.setAbsolutePosition(100f, 700f);
+//package com.example.ikramova.PDF;
+//import java.io.*;
+//import java.net.URL;
+//import java.io.FileOutputStream;
+//
+//import com.itextpdf.text.Document;
+//import com.itextpdf.text.DocumentException;
+//import com.itextpdf.text.Paragraph;
+//import com.itextpdf.text.pdf.*;
+//import com.itextpdf.text.*;
+//import com.itextpdf.text.pdf.PdfWriter;
+//public class Reading_and_Modifying_an_Existing_PDF {
+//    public static void main(String[] args) {
+//        try
+//        {
+//            //Read file using PdfReader
+//            PdfReader pdfReader = new PdfReader("HelloWorld.pdf");
+//            //Modify file using PdfReader
+//            PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileOutputStream("HelloWorld-modified.pdf"));
+//            Image image = Image.getInstance("temp.jpg");
+//            image.scaleAbsolute(100, 50);
+//            image.setAbsolutePosition(100f, 700f);
 //            for(int i = 1; i&lt;= pdfReader.getNumberOfPages(); i++)
 //            {
 //                PdfContentByte content = pdfStamper.getUnderContent(i);
 //                content.addImage(image);
 //            }
-            pdfStamper.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//            pdfStamper.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (DocumentException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- src/main/java/com/example/ikramova/PDF/Writing_PDF_to_HttpOutputStream.java
+++ src/main/java/com/example/ikramova/PDF/Writing_PDF_to_HttpOutputStream.java
@@ -1,28 +1,28 @@
-package com.example.ikramova.PDF;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.net.URL;
-import java.util.Date;
-
-import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.CMYKColor;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
-public class Writing_PDF_to_HttpOutputStream {
-    public static void main(String[] args) {
-        Document document = new Document();
-        try{
+//package com.example.ikramova.PDF;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+//import java.net.URL;
+//import java.util.Date;
+//
+//import com.itextpdf.text.*;
+//import com.itextpdf.text.pdf.CMYKColor;
+//import com.itextpdf.text.pdf.PdfPCell;
+//import com.itextpdf.text.pdf.PdfPTable;
+//import com.itextpdf.text.pdf.PdfWriter;
+//public class Writing_PDF_to_HttpOutputStream {
+//    public static void main(String[] args) {
+//        Document document = new Document();
+//        try{
 //            response.setContentType("application/pdf");
 //            PdfWriter.getInstance(document, response.getOutputStream());
-            document.open();
-            document.add(new Paragraph("howtodoinjava.com"));
-            document.add(new Paragraph(new Date().toString()));
-            //Add more content here
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        document.close();
-    }
-    }
-//}
+//            document.open();
+//            document.add(new Paragraph("howtodoinjava.com"));
+//            document.add(new Paragraph(new Date().toString()));
+//            //Add more content here
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//        document.close();
+//    }
+//    }
+////}
