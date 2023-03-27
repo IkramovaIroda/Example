1e182d2373ede68720950ae97b412bd9aeae6716
Ikramova Iroda <irodastudent@octo.uz>
2023-03-27 16:30:05 +0300

    task 922

---
+++ src/main/java/com/example/ikramova/PDF/Setting_File_Attributes_to_PDF.java
@@ -0,0 +1,34 @@
+package com.example.ikramova.PDF;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class Setting_File_Attributes_to_PDF {
+
+    public static void main(String[] args) {
+
+        Document document = new Document();
+        try {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("SetAttributeExample.pdf"));
+            document.open();
+            document.add(new Paragraph("Some content here"));
+
+            //Set attributes here
+            document.addAuthor("Lokesh Gupta");
+            document.addCreationDate();
+            document.addCreator("HowToDoInJava.com");
+            document.addTitle("Set Attribute Example");
+            document.addSubject("An example to show how attributes can be added to pdf files.");
+
+
+            document.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
