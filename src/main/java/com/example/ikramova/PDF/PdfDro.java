60a3a148e80669c11bdd76a43c6789ce655a9c20
Ikramova Iroda <irodastudent@octo.uz>
2023-03-29 16:51:27 +0300

    my commit

---
+++ src/main/java/com/example/ikramova/PDF/PdfDro.java
@@ -0,0 +1,16 @@
+package com.example.ikramova.PDF;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PdfDro {
+    private String card;
+    private String user;
+    private String reId;
+    private String phone_number;
+    private String card_provider;
+}
